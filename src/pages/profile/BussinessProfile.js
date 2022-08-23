f6a57e5ae687efc8040e8cd8a790b0fd566a49b9
reshul-ai <reshul@braina.live>
2022-08-22 18:39:54 -0700

    Business profile, student profile page ux updated

--- src/pages/profile/BussinessProfile.js
+++ src/pages/profile/BussinessProfile.js
@@ -1,100 +1,164 @@
 import React from "react";
 
 function BussinessProfile() {
+
+
+
+  let studentProfileDetails = {
+    name: "John Doe",
+    dob: "Demo Date of Birth",
+    bio:"Hi I am John Doe using BrainAlive B2B"
+};
+
+
   return (
-    <section className="container fluid" style={{ color: "red" }}>
-      <div className="row" style={{ display: "flex", flexDirection: "row" }}>
-        <div className="col-6">
-          <div>
-            <div className="row">
-              <div
-                className="col"
-                style={{
-                  padding: "10px",
-                  margin: "auto",
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
-                  style={{
-                    borderRadius: "50%",
-                    height: "200px",
-                    width: "200px",
-                    margin: "auto",
-                  }}
-                />
-              </div>
-              <div
-                className="col"
-                style={{
-                  padding: "10px",
-                  color: "white",
-                  paddingTop: "30px",
-                }}
-              >
-                <h2>Name</h2>
-                <h4>Email@gmail.com</h4>
-                <h5>D.O.B</h5>
-                <h5>Bio some yhing about me</h5>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-6" style={{ color: "white" }}>
-          <div className="row">
-            <div style={{ padding: "15px" }}>
-              <div style={{ display: "flex" }}>
-                <h4>Active Subscription</h4>
-                <h4
-                  style={{
-                    marginLeft: "10px",
-                    background: "green",
-                    borderRadius: "20px",
-                    padding: "5px",
-                    fontSize: "15px",
-                    width: "130px",
-                    textAlign: "center",
-                  }}
-                >
-                  Free Tier
-                </h4>
-              </div>
-              <div>
-                <div style={{ marginTop: "20px" }}>
-                  <div className="btn btn-outline-light">Renew Licence</div>
+    // <section className="container fluid" style={{ color: "red" }}>
+    //   <div className="row" style={{ display: "flex", flexDirection: "row" }}>
+    //     <div className="col-lg-6">
+    //       <div className="container-fluid">
+    //         <div className="row">
+    //           <div
+    //             className="col"
+    //             style={{
+    //               padding: "10px",
+    //               margin: "auto",
+    //               display: "flex",
+    //               justifyContent: "center",
+    //             }}
+    //           >
+                //  <img
+                //   src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+                //   className="img img-fluid w-75"
+                //   style={{
+                //     borderRadius: "50%",
+                //     margin: "auto",
+                //   }}
+                // />
+    //           </div>
+    //           <div
+    //             className="col"
+    //             style={{
+    //               padding: "10px",
+    //               color: "white",
+    //               paddingTop: "30px",
+    //             }}
+    //           >
+                //  <h2>Name</h2>
+                // <h4>{studentProfileDetails.name}</h4>
+                // <h5>{studentProfileDetails.dob}</h5>
+                // <h5>{studentProfileDetails.bio}</h5>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div className="col-lg-6" style={{ color: "white" }}>
+    //       <div className="row">
+    //         <div style={{ padding: "15px" }}>
+    //           <div style={{ display: "flex" }}>
+    //             <h4>Active Subscription</h4>
+    //             <h5 className="bg-success ms-2 ps-2 pe-2 border rounded">
+    //               Free Tier
+    //             </h5>
+    //           </div>
+    //           <div>
+    //             <div style={{ marginTop: "20px" }}>
+    //               <div className="btn btn-outline-light">Renew Licence</div>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <hr style={{ color: "white" }}></hr>
+    //     <div className="col-6" style={{ color: "white" }}>
+    //       <h2>Generated Tokens</h2>
+    //       <div style={{ marginTop: "20px" }}>
+    //         <div>
+              // <select>
+              //   <option>Enter user type</option>
+              //   <option>B2C</option>
+              //   <option>B2B</option>
+              // </select>
+    //         </div>
+    //         <div className="btn btn-outline-light">Genrate Token</div>
+    //       </div>
+    //       <div className="col-md-6" style={{ marginTop: "20px" }}>
+            // <ul>
+            //   <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
+            //   <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
+            //   <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
+            //   <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
+            //   <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
+            //   <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
+            // </ul>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </section>
+    <>
+          <section className="gradient-custom">
+              <div className="container-fluid mb-5 pb-5 pt-5">
+                <div className="row text-center text-light d-flex justify-content-center">
+                    <div className="col-sm-4 text-lg-end">
+                        < img
+                        src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+                        className="img img-fluid w-50"
+                        style={{
+                          borderRadius: "50%",
+                          margin: "auto",
+                        }}
+                      />
+                    </div>
+                    <div className="col-md-4 text-md-start">
+                            <h2>Name</h2>
+                        <h4>{studentProfileDetails.name}</h4>
+                        <h5>{studentProfileDetails.dob}</h5>
+                        <h5>{studentProfileDetails.bio}</h5>
+                    </div>
+                    <div className="col text-lg-start pt-5 pt-lg-0">
+                        <div className="container-fluid">
+                            <div className="row text-md-start">
+                              <div className="col">
+                              <h4>Active Subscription</h4>
+                       
+                              </div>
+                              <div className="col-4 text-start">
+                              <p className="bg-success ms-2 ps-2 pe-2 border rounded">Free Tier</p>
+                              </div>
+                            </div>
+                            <div className="row">
+                              <div className="col">
+                                  <button className="btn btn-outline-light">Renew Lisence</button>
+                              </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr className="text-light" />
+                <div className="row text-center text-light d-flex justify-content-center">
+                        <div className="col-md-6 text-md-end">
+                          <select>
+                            <option>Enter user type</option>
+                            <option>B2C</option>
+                            <option>B2B</option>
+                          </select>
+                          <div className="btn btn-outline-light">Genrate Token</div>
+                        </div>
+                        <div className="col-md-6 pt-lg-0 pt-5 text-lg-start">
+                        <ul>
+                            <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
+                            <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
+                            <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
+                            <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
+                            <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
+                            <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
+                        </ul>
+                        </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
-        <hr style={{ color: "white" }}></hr>
-        <div className="col-6" style={{ color: "white" }}>
-          <h2>Genrated Tokens</h2>
-          <div style={{ marginTop: "20px" }}>
-            <div>
-              <select>
-                <option>Enter user type</option>
-                <option>B2C</option>
-                <option>B2B</option>
-              </select>
-            </div>
-            <div className="btn btn-outline-light">Genrate Token</div>
-          </div>
-          <div style={{ marginTop: "20px" }}>
-            <ul>
-              <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
-              <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
-              <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
-              <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
-              <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
-              <li>hgsakjdhdkjshjfskldkjsdkljdkjsfah</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </section>
+          </section>
+    </>
+    
   );
 }
 
--- src/pages/profile/StudentProfile.js
+++ src/pages/profile/StudentProfile.js
@@ -1,16 +1,59 @@
 import React from "react";
 
 function StudentProfile() {
+  
+
+  let studentProfileDetails = {
+      name: "John Doe",
+      dob: "Demo Date of Birth",
+      bio:"Hi I am John Doe using BrainAlive B2B"
+  };
+
+  let upcomingClasses = [
+    {
+      id: 1,
+      date: "Demo Date 1",
+      name:"Demo Class Name 1",
+      agenda: "Demo Agenda 1"
+    },
+    {
+      id: 2,
+      date: "Demo Date 2",
+      name:"Demo Class Name 2",
+      agenda: "Demo Agenda 2"
+    },
+    {
+      id: 3,
+      date: "Demo Date 3",
+      name:"Demo Class Name 3",
+      agenda: "Demo Agenda 3"
+    },
+    {
+      id: 4,
+      date: "Demo Date 4",
+      name:"Demo Class Name 4",
+      agenda: "Demo Agenda 4"
+    },
+    {
+      id: 5,
+      date: "Demo Date 5",
+      name:"Demo Class Name 5",
+      agenda: "Demo Agenda 5"
+    }
+  ]
+
+  
   return (
-    <section className="container fluid">
-      <div className="row">
-        <div className="col-6">
-          <div style={{}}>
-            <div className="row">
+    <>
+          <section className="gradient-custom">
+              <div className="container-fluid">
+              <div className="row d-flex justify-content-around align-items-center">
+        <div className="col col-12 col-sm-12 col-md-7 col-lg-5">
+          <div className="container-fluid">
+            <div className="row text-center">
               <div
-                className="col"
+                className="col-md"
                 style={{
-                  padding: "10px",
                   margin: "auto",
                   display: "flex",
                   justifyContent: "center",
@@ -18,119 +61,62 @@ function StudentProfile() {
               >
                 <img
                   src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+                  className="img img-fluid w-50"
                   style={{
                     borderRadius: "50%",
-                    height: "200px",
-                    width: "200px",
                     margin: "auto",
                   }}
                 />
               </div>
-              <div
-                className="col"
-                style={{
+              <div className="col-md-8 text-md-start" style={{
                   padding: "10px",
                   color: "white",
                   paddingTop: "30px",
-                }}
-              >
+                }}>
                 <h2>Name</h2>
-                <h4>Email@gmail.com</h4>
-                <h5>D.O.B</h5>
-                <h5>Bio some yhing about me</h5>
+                <h4>{studentProfileDetails.name}</h4>
+                <h5>{studentProfileDetails.dob}</h5>
+                <h5>{studentProfileDetails.bio}</h5>
               </div>
             </div>
           </div>
         </div>
         {/* <div className="col-6">hii</div> */}
-      </div>
-      <hr style={{ color: "white" }} />
-      <div style={{ color: "white" }}>
-        <h2>Upcoming Classes</h2>
-      </div>
-      <div className="row" style={{ marginTop: "20px", color: "white" }}>
-        <div
-          className="col-3"
-          style={{ marginTop: "10px", marginBottom: "10px" }}
-        >
-          <div
-            style={{
-              padding: "15px",
-              borderRadius: "20px",
-              border: "1px solid white",
-            }}
-          >
-            <h3>Date</h3>
-            <h5>Name</h5>
-            <h6>Agenda this is more than oneline</h6>
           </div>
-        </div>
-        <div
-          className="col-3"
-          style={{ marginTop: "10px", marginBottom: "10px" }}
-        >
-          <div
-            style={{
-              padding: "15px",
-              borderRadius: "20px",
-              border: "1px solid white",
-            }}
-          >
-            <h3>Date</h3>
-            <h5>Name</h5>
-            <h6>Agenda this is more than oneline</h6>
+            <hr style={{ color: "white" }} />
+            <div style={{ color: "white" }}>
+              <h2>Upcoming Classes</h2>
+            </div>
+          <div className="row text-light"  >
+                  {
+                    upcomingClasses.map(item =>{
+                      
+                      return(
+                        <div
+                        className="col-lg-3 col-md-4 col-sm-4"
+                            style={{ marginTop: "10px", marginBottom: "10px" }}
+                          >
+                            <div
+                              style={{
+                                padding: "15px",
+                                borderRadius: "20px",
+                                border: "1px solid white",
+                              }}
+                            >
+                              <h3>{item.date}</h3>
+                              <h5>{item.name}</h5>
+                              <h6>{item.agenda}</h6>
+                            </div>
+                          </div>
+                      )
+
+                      
+                    })
+                  }            
           </div>
-        </div>
-        <div
-          className="col-3"
-          style={{ marginTop: "10px", marginBottom: "10px" }}
-        >
-          <div
-            style={{
-              padding: "15px",
-              borderRadius: "20px",
-              border: "1px solid white",
-            }}
-          >
-            <h3>Date</h3>
-            <h5>Name</h5>
-            <h6>Agenda this is more than oneline</h6>
           </div>
-        </div>
-        <div
-          className="col-3"
-          style={{ marginTop: "10px", marginBottom: "10px" }}
-        >
-          <div
-            style={{
-              padding: "15px",
-              borderRadius: "20px",
-              border: "1px solid white",
-            }}
-          >
-            <h3>Date</h3>
-            <h5>Name</h5>
-            <h6>Agenda this is more than oneline</h6>
-          </div>
-        </div>
-        <div
-          className="col-3"
-          style={{ marginTop: "10px", marginBottom: "10px" }}
-        >
-          <div
-            style={{
-              padding: "15px",
-              borderRadius: "20px",
-              border: "1px solid white",
-            }}
-          >
-            <h3>Date</h3>
-            <h5>Name</h5>
-            <h6>Agenda this is more than oneline</h6>
-          </div>
-        </div>
-      </div>
-    </section>
+          </section>
+    </>
   );
 }
 
